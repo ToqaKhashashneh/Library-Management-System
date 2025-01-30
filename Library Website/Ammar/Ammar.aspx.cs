b1dcb5fe55eec95bb72cfc16c38db155a4add1b6
Toqa Khashashneh <toqamusa@gmail.com>
2025-01-30 18:53:14 +0300

    project

---
+++ Library Website/Ammar/Ammar.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Library_Website.Ammar
+{
+    public partial class Ammar : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
